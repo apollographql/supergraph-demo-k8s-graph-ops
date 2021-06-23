d53f4402b5a812248ef0b1f839f9ae61b2274c26
Phil Prasek <prasek@gmail.com>
2021-06-22 20:26:17 -0700

    ci-matrix pull request
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .scripts/ci-matrix.sh
+++ .scripts/ci-matrix.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
-COMMON_THINGS=(".scripts" ".github/workflows" "Makefile" "clusters/kind-cluster.yaml")
-
-ENVIRONMENTS=("dev" "stage" "prod")
-THINGS=("infra" "subgraphs" "router")
-
 GITHUB_SHA="${GITHUB_SHA:-HEAD}"
 
 if [[ "$1" != "local" ]]; then
   GITHUB_EVENT_BEFORE=${GITHUB_EVENT_BEFORE:-HEAD^}
 fi
 
-if [[ -n "$GITHUB_EVENT_BEFORE" ]]; then
-  # only fetch in CI if not present
-  if [[ "$(git cat-file -t $GITHUB_EVENT_BEFORE)" != "commit" ]]; then
-    >&2 echo "fetching GITHUB_EVENT_BEFORE: $GITHUB_EVENT_BEFORE"
-    git fetch origin $GITHUB_EVENT_BEFORE --depth=1
-  fi
-fi
-
->&2 echo "found GITHUB_EVENT_BEFORE: $GITHUB_EVENT_BEFORE"
->&2 echo "found GITHUB_SHA: $GITHUB_SHA"
-
 # for dynamic build matrix in GitHub actions, see:
 # https://github.community/t/check-pushed-file-changes-with-git-diff-tree-in-github-actions/17220/10
+
+if [[ -n "$GITHUB_BASE_REF" ]]; then
+  # Pull Request
+  >&2 echo "fetching GITHUB_BASE_REF: $GITHUB_BASE_REF"
+  git fetch origin $GITHUB_BASE_REF --depth=1
+else
+  # Push
+  if [[ -n "$GITHUB_EVENT_BEFORE" ]]; then
+    # only fetch in CI if not present
+    if [[ "$(git cat-file -t $GITHUB_EVENT_BEFORE)" != "commit" ]]; then
+      >&2 echo "fetching GITHUB_EVENT_BEFORE: $GITHUB_EVENT_BEFORE"
+      git fetch origin $GITHUB_EVENT_BEFORE --depth=1
+    fi
+  fi
+
+  >&2 echo "found GITHUB_EVENT_BEFORE: $GITHUB_EVENT_BEFORE"
+  >&2 echo "found GITHUB_SHA: $GITHUB_SHA"
+fi
+
+function diff_name_only() {
+  if [[ -n "$GITHUB_BASE_REF" ]]; then
+    # Pull Request
+    git diff --name-only origin/$GITHUB_BASE_REF $GITHUB_SHA $1
+  else
+    # Push
+    git diff --name-only $GITHUB_EVENT_BEFORE $GITHUB_SHA $1
+  fi
+}
+
+COMMON_THINGS=(".scripts" ".github/workflows" "Makefile" "clusters/kind-cluster.yaml")
+
+ENVIRONMENTS=("dev" "stage" "prod")
+THINGS=("infra" "subgraphs" "router")
 
 TMPFILE=$(mktemp)
 
